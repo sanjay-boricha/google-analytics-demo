4bbcc89c5c782eb3e299e82b3a4d0753c208614c
sanjay-boricha <sanjay@artixun.com>
2021-07-22 17:54:30 +0530

    update

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="py-5">
-    <nuxt-link class="mx-3" to="/">Home</nuxt-link>
-    <nuxt-link class="mx-3" to="/docs">Docs</nuxt-link>
-    <nuxt-link class="mx-3" to="/about">About</nuxt-link>
+    <nuxt-link class="mx-3 text-blue-500" to="/">Home</nuxt-link>
+    <nuxt-link class="mx-3 text-blue-500" to="/docs">Docs</nuxt-link>
+    <nuxt-link class="mx-3 text-blue-500" to="/about">About</nuxt-link>
   </nav>
 </template>
 
