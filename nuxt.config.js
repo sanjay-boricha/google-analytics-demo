b7411d841a0d2645ada88cc67daebda114afeb5a
sanjay-boricha <sanjay@artixun.com>
2021-07-22 15:44:53 +0530

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'server',
-  ssr: false,
+  target: 'static',
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
